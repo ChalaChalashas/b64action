0542c25ff116ed8be97eb92e95df6f7966a5e1a7
ChalaChalashas <p.kaunietis@gmail.com>
2022-12-18 23:00:45 +0000

    some more tests

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ const fs = require('fs');
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
-    const filePath = untildify(core.getInput('filePath'));    
+    const filePath = untildify(core.getInput('filePath'));  
+    core.debug("-----------------------------------------------------------------------------------------------------");  
+    core.debug("path");
     core.debug(filePath);
     let np = path.normalize(filePath);
+    core.debug("np");
     core.debug(np);
     let f = fs.readFileSync(np);
     let b64 = bota(f);
+    core.debug("b64");
     core.debug(b64);
+    core.debug("-----------------------------------------------------------------------------------------------------");  
     let promise = new Promise(function(resolve, reject) {
       base64.encode(path.normalize(filePath), function(err, base64String) {
         if(err){
